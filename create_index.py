0fcc1010e38d83fb5f038b6500f215e2fc31638c
dagopa <puad__91>
2021-12-10 10:49:11 -0600

    create index

--- create_index.py
+++ create_index.py
@@ -1,13 +1,22 @@
+from os import path
 import pandas as pd
-
-
-input_path = 'data'
 
 municipios = ['Aguascalientes', 'Campeche', 'Centro', 'Chihuahua', 'Chilpancingo de los Bravo', 'Colima', 'Cuernavaca',
               'Culiacán', 'Durango', 'Guadalajara', 'Guanajuato', 'Hermosillo', 'La Paz', 'Mexicali', 'Monterrey',
               'Morelia', 'Mérida', 'Oaxaca de Juárez', 'Othón P. Blanco', 'Pachuca de Soto', 'Puebla', 'Querétaro',
               'Saltillo', 'San Luis Potosí', 'Tepic', 'Tlaxcala', 'Toluca', 'Tuxtla Gutiérrez', 'Victoria', 'Xalapa',
               'Zacatecas']
+
+
+
+data_path = 'data'
+municipio = municipios[0]
+
+asc = pd.read_csv(path.join(data_path, municipio + '_ASCENDING_.csv')).iloc[:, 1].to_numpy()
+dsc = pd.read_csv(path.join(data_path, municipio + '_DESCENDING_.csv')).iloc[:, 1].to_numpy()
+
+changes = asc + dsc / 2
+
 
 
 print('eof')
