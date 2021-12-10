a090ef8088c70bfe5a5f1028e7916827388efddc
dagopa <dagopuar@hotmail.com>
2021-12-10 07:17:01 -0600

    Run experiment

---
+++ create_index.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+input_path = 'data'
+
+municipios = ['Aguascalientes', 'Campeche', 'Centro', 'Chihuahua', 'Chilpancingo de los Bravo', 'Colima', 'Cuernavaca',
+              'Culiacán', 'Durango', 'Guadalajara', 'Guanajuato', 'Hermosillo', 'La Paz', 'Mexicali', 'Monterrey',
+              'Morelia', 'Mérida', 'Oaxaca de Juárez', 'Othón P. Blanco', 'Pachuca de Soto', 'Puebla', 'Querétaro',
+              'Saltillo', 'San Luis Potosí', 'Tepic', 'Tlaxcala', 'Toluca', 'Tuxtla Gutiérrez', 'Victoria', 'Xalapa',
+              'Zacatecas']
+
+
+print('eof')
