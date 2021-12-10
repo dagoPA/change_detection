Fixed weighting, added new column

create_index.py
@@ -28,8 +28,7 @@ for i in range(len(municipios)):
     asc = pd.read_csv(path.join(data_path + '/gee_results', municipio.iloc[0] + '_ASCENDING_.csv')).iloc[:, 1].to_numpy()
     dsc = pd.read_csv(path.join(data_path + '/gee_results', municipio.iloc[0] + '_DESCENDING_.csv')).iloc[:, 1].to_numpy()
     changes = (asc + dsc) / 2
-    weighted_changes = (changes * municipio['weight']).tolist()
-    data_output.append(weighted_changes)
+    data_output.append(changes)
     headers.append(municipio.iloc[0])
 
 # Convert to numpy and flip over the diagonal
@@ -44,6 +43,16 @@ data_output = np.abs(normalize(data_output, axis=0))
 
 data_output = pd.DataFrame(data=data_output, index=dates, columns=headers)
 
+# Create national weighted index
+nwci = []
+for index, row in data_output.iterrows():
+    monthly_total = 0
+    for i, v in row.iteritems():
+        monthly_total = monthly_total + (v * municipios[municipios['capital'] == i]['weight'])
+
+    nwci.append(monthly_total)
+
 # Save as csv
+data_output['national_weighted_index'] = nwci
 data_output.to_csv(path.join(data_path, 'normalized_change_index.csv'))
 print('eof')