1e1b45d894364fc98af1f49e70e7b9940c75f1fc
dagopa <dagopuar@hotmail.com>
2022-01-04 16:29:20 -0600

    fixed bug

--- changes_capital_runable.py
+++ changes_capital_runable.py
@@ -7,19 +7,16 @@ import geeTools as geet
 
 # Begin Params
 municipio = sys.argv[1]
-export_result = (sys.argv[2] == 'True')
-export = (sys.argv[3] == 'True')
-sum_values = (sys.argv[4] == 'True')
-start_date = sys.argv[5]
-end_date = sys.argv[6]
-local_data_dir = sys.argv[7]
+export_result = sys.argv[2] == 'True'
+export = sys.argv[3] == 'True'
+sum_values = sys.argv[4] == 'True'
 
 orbits = ['ASCENDING', 'DESCENDING']
 gdrive_folder = 'changes_cdmx_1m'
+start_date = '2017-01-01'
+end_date = '2022-03-01'
 frequency = '1M'
-# start_date = '2017-01-01'
-# end_date = '2021-12-01'
-# local_data_dir = 'data'
+local_data_dir = 'data/gee_results'
 # End Params
 
 # Get list of dates
@@ -30,25 +27,22 @@ dates = dates.strftime("%Y-%m-%d").values.tolist()
 capitales = ee.FeatureCollection("projects/ee-vulnerability-gee4geo/assets/capitales")
 
 for orbit_ in orbits:
-    my_file = Path(local_data_dir + municipio + '_' + orbit_ + '_' + '.csv')
-    if not my_file.is_file():
-        # Filter municipio and convert it to geometry in case it's needed
-        filter_ee = ee.Filter.inList('NOMGEO', [municipio])
-        capital = capitales.filter(filter_ee).first().geometry()
+    # Filter municipio and convert it to geometry in case it's needed
+    filter_ee = ee.Filter.inList('NOMGEO', [municipio])
+    capital = capitales.filter(filter_ee).first().geometry()
 
-        if capital.name() != 'Geometry':
-            capital = capital.geometries().get(1)
+    if capital.name() != 'Geometry':
+        capital = capital.geometries().get(1)
 
-        changes = []
-        # Calculate changes, comparing each month against the next one
-        for i in range(2, len(dates)):
-            sum = geet.calculate_monthly_changes(dates[i - 2], dates[i - 1], dates[i], capital, orbit_, 'test_SAR',
-                                                 export=export, export_result=export_result, sum_values=sum_values)
-            changes.append(sum)
+    changes = []
+    # Calculate changes, comparing each month against the next one
+    for i in range(2, len(dates)):
+        sum = geet.calculate_monthly_changes(dates[i - 2], dates[i - 1], dates[i], capital, orbit_, 'test_SAR',
+                                             export=export, export_result=export_result, sum_values=sum_values)
+        changes.append(sum)
 
-        if sum_values:
-            dict = {municipio + '_' + orbit_: changes}
-            df = pd.DataFrame(dict)
-            df.to_csv(local_data_dir + '/' + municipio + '_' + orbit_ + '_' + '.csv')
+    dict = {municipio + '_' + orbit_: changes}
+    df = pd.DataFrame(dict)
+    df.to_csv(local_data_dir + '/' + municipio + '_' + orbit_ + '_' + '.csv')
 
 print('end of file')
