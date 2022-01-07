c6ec48cc67e9ccb22d3103146b399ccb79d4a732
dagopa <dagopuar@hotmail.com>
2022-01-07 12:39:52 -0600

    Generates dataset from one image

--- prepare_crop_dataset.py
+++ prepare_crop_dataset.py
@@ -2,24 +2,34 @@ import numpy as np
 import geeTools
 import matplotlib.pyplot as plt
 import random
-import matplotlib.patches as patches
+from PIL import Image
 
 random.seed(128)
 # args
-plot = True
+plot = False
 im_size = 256
 # read image
 data_path = '/home/dagopa/data/SAR/'
+
 im = geeTools.readGeoTiffAsRGB(data_path + 'SAR_2019-07-01_2019-08-01.tif')[0]
 
-A = []
-B = []
-C = []
 # split image into slidding windows patches
-windows = geeTools.sliding_window(im, 1, (im_size, im_size))
+c = 1
+windows = geeTools.sliding_window(im, 50, (im_size, im_size))
 for window in windows:
     im_A = window[2]
     if not np.isnan(im_A).any():
+
+        # normalize between 0 and 255
+        # for bands 0 (HH )and 1 (HV) min is -50 and max is 1
+        # for band 2 (angle) min is 0 and max is 90
+
+        im_A = np.uint8(im_A)
+
+        im_A[:, :, 0] = (im_A[:, :, 0] + 50) / 51 * 255
+        im_A[:, :, 1] = (im_A[:, :, 1] + 50) / 51 * 255
+        im_A[:, :, 2] = (im_A[:, :, 2] / 90) * 255
+
         # prepare arrays for storing results
         im_width = im_A.shape[0]
         im_height = im_A.shape[1]
@@ -28,22 +38,32 @@ for window in windows:
 
         # random synthetic changes
 
-        # number of pixels that surround the center of the changed clip
-        patch_width = random.randint(2, 3)
-        patch_height = random.randint(patch_width + 0, patch_width)
+        n_changes = random.randint(10, 50)
 
-        # origin of the patch
-        x_o = random.randint(patch_width, im_width - patch_width) - patch_width
-        y_o = random.randint(patch_height, im_height - patch_height) - patch_height
-        # destination of the patch
-        x_d = random.randint(patch_width, im_width - patch_width) - patch_width
-        y_d = random.randint(patch_height, im_height - patch_height) - patch_height
+        for i in range(n_changes):
+            # number of pixels that surround the center of the changed clip
+            patch_width = random.randint(2, 3)
+            patch_height = random.randint(patch_width, patch_width)
 
-        # perform the change
-        patch = im_A[x_o - patch_width:x_o + patch_width, y_o - patch_height:y_o + patch_height, 0:2]
-        im_B[x_d - patch_width:x_d + patch_width, y_d - patch_height:y_d + patch_height, 0:2] = patch
+            # origin of the patch
+            x_o = random.randint(patch_width+1, im_width - patch_width) - patch_width
+            y_o = random.randint(patch_height+1, im_height - patch_height) - patch_height
+            # destination of the patch
+            x_d = random.randint(patch_width+1, im_width - patch_width) - patch_width
+            y_d = random.randint(patch_height+1, im_height - patch_height) - patch_height
 
-        im_l[x_d - patch_width:x_d + patch_width, y_d - patch_height:y_d + patch_height] = 1
+            # perform the change
+            patch = im_A[x_o - patch_width:x_o + patch_width, y_o - patch_height:y_o + patch_height, 0:2]
+            try:
+                im_B[x_d - patch_width:x_d + patch_width, y_d - patch_height:y_d + patch_height, 0:2] = patch
+            except:
+                print('error')
+
+            im_l[x_d - patch_width:x_d + patch_width, y_d - patch_height:y_d + patch_height] = 255
+
+
+            print('patch completed')
+
 
         # plot
         if plot:
@@ -60,8 +80,16 @@ for window in windows:
             plt.show()
 
         # save the images
+        A = Image.fromarray(im_A)
+        A.save(data_path + 'A/im' + str(c) + '.png')
 
-        print('completed')
+        B = Image.fromarray(im_B)
+        B.save(data_path + 'B/im' + str(c) + '.png')
 
+        # im_l = np.dstack((im_l, im_l, im_l))
+        label = Image.fromarray(np.uint8(im_l))
+        label.save(data_path + 'labels/im' + str(c) + '.png')
+
+        c = c + 1
 
 print('eof')
