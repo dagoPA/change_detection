af6e05c545f5282cb32d1b7bc271f41be58a53e6
dagopa <dagopuar@hotmail.com>
2022-02-14 14:44:23 -0600

    Export municipalities

---
+++ export_municipalities.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import ee
+import geeTools as geet
+import pandas as pd
+# Begin Params
+
+orbits = ['ASCENDING', 'DESCENDING']
+gdrive_folder = 'cdmx_2021'
+initial_date = '2022-01-01'
+final_date = '2022-02-12'
+local_data_dir = 'data/gee_results'
+frequency = 14
+# End Params
+
+# Get the polygon of mexico City
+cdmx = ee.FeatureCollection("projects/ee-vulnerability-gee4geo/assets/cdmx").first().geometry()
+
+
+geet.calculate_changes(initial_date, final_date, 'both', frequency, cdmx, gdrive_folder, file_prefix='changes_cdmx', export_result=False, export=True)
+
+print('end of file')
