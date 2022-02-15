538ea2232dc6889808c861f2b95427301577f584
dagopa <dagopuar@hotmail.com>
2022-02-14 18:11:24 -0600

    Export 2020 - 2022

--- tjom_cdmx.py
+++ tjom_cdmx.py
@@ -4,10 +4,10 @@ import geeTools as geet
 import pandas as pd
 # Begin Params
 
-orbits = ['ASCENDING', 'DESCENDING']
+orbits = ['ASCENDING', 'DESCENDING', 'both']
 gdrive_folder = 'cdmx_2021'
-initial_date = '2022-01-01'
-final_date = '2022-02-12'
+initial_date = '2020-01-01'
+final_date = '2022-01-01'
 local_data_dir = 'data/gee_results'
 frequency = 14
 # End Params
@@ -15,6 +15,6 @@ frequency = 14
 # Get the polygon of mexico City
 cdmx = ee.FeatureCollection("projects/ee-vulnerability-gee4geo/assets/cdmx").first().geometry()
 
-geet.calculate_changes(initial_date, final_date, 'both', frequency, cdmx, gdrive_folder, file_prefix='changes_cdmx', export_result=False, export=True)
+geet.calculate_changes(initial_date, final_date, orbits[0], frequency, cdmx, gdrive_folder, file_prefix='changes_cdmx', export_result=False, export=True)
 
 print('end of file')
